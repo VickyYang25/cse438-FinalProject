82aa54cc2f84d17ec11e77ca36295652dab43fdf
VickyYang25 <keyan@wustl.edu>
2023-11-03 16:57:42 -0500

    11/3 added new xcode

---
+++ Tourify/Tourify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tourify
+//
+//  Created by 杨可嫣 on 11/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
